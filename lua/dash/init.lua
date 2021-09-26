1de075026fa5d5c8c2f1d0c5072c9064785a528d
Mat Jones <mat@mjones.network>
2021-09-25 20:02:58 -0400

    WIP: experiment with sorting by current buffer type

--- lua/dash/init.lua
+++ lua/dash/init.lua
@@ -1,35 +1,7 @@
 local M = {}
 
--- lazy-constructed Telescope picker
-local picker = nil
-
-local function getPicker()
-  if picker ~= nil then
-    return picker
-  end
-
-  local Picker = require('telescope.pickers')
-  local Finder = require('telescope.finders')
-  local Sorter = require('telescope.sorters')
-  local finderUtils = require('dash.utils.telescope')
-  local finder = Finder.new_dynamic({
-    fn = finderUtils.finderFn,
-    entry_maker = finderUtils.entryMaker,
-    on_complete = {},
-  })
-
-  picker = Picker:new({
-    prompt_title = 'Dash',
-    finder = finder,
-    sorter = Sorter.get_generic_fuzzy_sorter(),
-    attach_mappings = finderUtils.attachMappings,
-  })
-
-  return picker
-end
-
 function M.search()
-  getPicker():find()
+  require('dash.utils.telescope').buildPicker():find()
 end
 
 function M.setup(config)
