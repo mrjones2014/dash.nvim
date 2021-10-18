afa0bd7b081012c1d03cf6d85c22b967316caaab
Mat Jones <mat@mjones.network>
2021-10-18 07:58:08 -0400

    resolve type errors

--- src/lib.rs
+++ src/lib.rs
@@ -29,6 +29,7 @@ struct LuaTableShim(*const c_void);
 unsafe impl Send for LuaTableShim {}
 unsafe impl Sync for LuaTableShim {}
 
+#[derive(Clone)]
 pub struct QueryParams {
     pub cli_path: &'static String,
     pub queries: &'static Vec<String>,
@@ -40,43 +41,44 @@ impl mlua::UserData for QueryParams {
     fn add_methods<'lua, M: LuaUserDataMethods<'lua, Self>>(_methods: &mut M) {}
 }
 
-impl Clone for QueryParams {
-    fn clone(&self) -> Self {
-        return QueryParams {
-            cli_path: &self.cli_path.to_string(),
-            queries: &self.queries.clone(),
-        };
-    }
-}
-
-fn query_sync(params: &'static QueryParams) -> Vec<TelescopeItem> {
+fn query_sync(params: QueryParams) -> Vec<TelescopeItem> {
     let (tx, rx) = channel::bounded(1);
-    let handle = Runtime::new().unwrap().handle();
+    let runtime = Runtime::new().unwrap();
+    let handle = runtime.handle();
     handle.spawn(async move {
         let result_table = &query(&params.cli_path, &params.queries).await;
-        let _ = tx.send(&result_table);
+        let _ = tx.send(result_table.clone());
     });
     return rx.recv().unwrap().to_owned().to_vec();
 }
 
-fn query_sync_lua_table<'a>(
-    lua: &'a Lua,
-    params: &'static QueryParams,
-) -> Result<LuaTable<'a>, LuaError> {
-    let result_telescope_items = query_sync(&params);
+fn query_sync_lua_table<'a>(lua: &'a Lua, params: QueryParams) -> Result<LuaTable<'a>, LuaError> {
+    let result_telescope_items = query_sync(params);
     let mut lua_table_items: Vec<LuaTable> = Vec::new();
     let lua_result_list: LuaTable = lua.create_table().unwrap();
     let mut i = 1;
     result_telescope_items.iter().for_each(|result| {
-        let result_lua_table = lua.create_table().unwrap();
-        result_lua_table.set("value", result.value.to_string());
-        result_lua_table.set("ordinal", result.ordinal.to_string());
-        result_lua_table.set("display", result.display.to_string());
-        result_lua_table.set("keyword", result.keyword.to_string());
-        result_lua_table.set("query", result.query.to_string());
+        let result_lua_table = &lua.create_table().unwrap();
+        result_lua_table
+            .set("value", result.value.to_string())
+            .unwrap();
+        result_lua_table
+            .set("ordinal", result.ordinal.to_string())
+            .unwrap();
+        result_lua_table
+            .set("display", result.display.to_string())
+            .unwrap();
+        result_lua_table
+            .set("keyword", result.keyword.to_string())
+            .unwrap();
+        result_lua_table
+            .set("query", result.query.to_string())
+            .unwrap();
 
-        lua_table_items.push(result_lua_table);
-        lua_result_list.raw_insert(i, result_lua_table.to_owned());
+        lua_table_items.push(result_lua_table.to_owned());
+        lua_result_list
+            .raw_insert(i, result_lua_table.to_owned())
+            .unwrap();
         i = i + 1;
     });
 
@@ -86,6 +88,8 @@ fn query_sync_lua_table<'a>(
 #[mlua::lua_module]
 fn dash_runner(lua: &Lua) -> LuaResult<LuaTable> {
     let exports = lua.create_table().unwrap();
-    exports.set("query", lua.create_function(query_sync_lua_table).unwrap());
+    exports
+        .set("query", lua.create_function(query_sync_lua_table).unwrap())
+        .unwrap();
     return Ok(exports);
 }
