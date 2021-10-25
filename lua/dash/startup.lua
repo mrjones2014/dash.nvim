b8f32a5d3f6e0023917f5adec3a32f11293342e2
Mat Jones <mat@mjones.network>
2021-10-25 06:59:41 -0400

    Add startup initialization check

---
+++ lua/dash/startup.lua
@@ -0,0 +1,20 @@
+local M = {}
+
+function M.init()
+  -- check if `make install` was run
+  local ok, libdash = pcall(require, 'libdash_nvim')
+  if not ok or libdash == nil then
+    vim.api.nvim_err_writeln(
+      'module "libdash_nvim" not found, did you set up Dash.nvim with `make install` as a post-install hook?'
+        .. ' See :h dash-install'
+    )
+    return
+  end
+
+  require('telescope._extensions.dash')
+  require('telescope').load_extension('dash')
+
+  vim.g.loaded_dash_vim = true
+end
+
+return M
