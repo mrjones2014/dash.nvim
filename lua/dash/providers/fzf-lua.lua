65ea8549f3ccdf4d1702685907190000e229f78a
Mat Jones <mat@mjones.network>
2021-10-31 18:02:21 -0400

    tweak options

--- lua/dash/providers/fzf-lua.lua
+++ lua/dash/providers/fzf-lua.lua
@@ -1,20 +1,19 @@
 local M = {}
 
-local _cached_results = {}
+local cached_results = {}
 
 local function handle_selected(selected)
-  if not selected or #selected ~= 1 or not _cached_results or #_cached_results < 1 then
+  if not selected or #selected ~= 1 or not cached_results or #cached_results < 1 then
     return
   end
 
   local matching_items = vim.tbl_filter(function(item)
     return item.display == selected[1]
-  end, _cached_results)
+  end, cached_results)
   if not matching_items or #matching_items < 1 then
     return
   end
   local selected_item = matching_items[1]
-  print(vim.inspect(selected_item))
   local libdash = require('libdash_nvim')
   if selected_item.is_fallback then
     libdash.open_search_engine(selected_item.value)
@@ -26,21 +25,23 @@ end
 
 local default_opts = {
   exec_empty_query = true,
+  bang = false,
   actions = {
     default = handle_selected,
   },
 }
 
-M.dash = function(bang)
+M.dash = function(opts)
   local fzf_lua = require('fzf-lua')
-  local opts = {}
   opts = fzf_lua.config.normalize_opts(opts, default_opts)
   if not opts then
     return
   end
 
   opts.prompt = 'Dash> '
-  opts.fzf_opts = { ['--header'] = vim.fn.shellescape(require('dash.providers').build_picker_title(bang)) }
+  opts.fzf_opts = {
+    ['--header'] = vim.fn.shellescape(require('dash.providers').build_picker_title(opts.bang or false)),
+  }
 
   -- This gets called whenever input is changed
   -- Also gets called first run if `opts.exec_empty_query == true`
@@ -50,10 +51,9 @@ M.dash = function(bang)
       return {}
     end
 
-    local results = require('libdash_nvim').query(query, current_file_type, bang or false)
-    _cached_results = results
+    cached_results = require('libdash_nvim').query(query, current_file_type, opts.bang or false)
     local items = {}
-    for _, item in pairs(results) do
+    for _, item in pairs(cached_results) do
       setmetatable(item, {
         __tostring = function(self)
           return self.display
