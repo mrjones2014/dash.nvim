c478b5f9d87d1e256d241bda751836baad0702ae
Mat Jones <mat@mjones.network>
2021-11-19 12:55:38 -0500

    remove old files

--- src_old/constants.rs
+++
@@ -1,3 +0,0 @@
-pub const DASH_APP_BASE_PATH: &str = "/Applications/Dash.app";
-pub const DASH_APP_CLI_PATH: &str = "/Contents/Resources/dashAlfredWorkflow";
-pub const KEYWORD_PATTERN: &str = r"^([a-zA-Z]+):.+";
