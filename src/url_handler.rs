b63f81b253afe77838d08fc599f0bb3783a9c88f
Mat Jones <mat@mjones.network>
2021-11-19 13:33:35 -0500

    update doc comments

--- src/url_handler.rs
+++ src/url_handler.rs
@@ -1,10 +1,12 @@
-/// Opens a URL on MacOS. Uses MacOS `open` command.
+/// Opens a URL. On MacOS, it uses the `open` command, on Linux, it uses the `xdg-open` command.
+/// Does not support Windows.
 #[cfg(target_os = "macos")]
 pub fn open_url(url: String) {
     let _ = std::process::Command::new("open").arg(url).output();
 }
 
-/// Opens a URL on Linux. Uses Linux `xdg-open` command.
+/// Opens a URL. On MacOS, it uses the `open` command, on Linux, it uses the `xdg-open` command.
+/// Does not support Windows.
 #[cfg(target_os = "linux")]
 pub fn open_url(url: String) {
     let _ = std::process::Command::new("xdg-open").arg(url).output();
