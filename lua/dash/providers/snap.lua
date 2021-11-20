7dabb18e4d705c196f0ef466800fe1143512631e
Mat Jones <mat@mjones.network>
2021-11-20 09:28:20 -0500

    fix snap provider param parsing

--- lua/dash/providers/snap.lua
+++ lua/dash/providers/snap.lua
@@ -10,7 +10,6 @@ end
 local function build_producer(current_file_type, bang)
   return function(request)
     if not request or request.canceled() then
-      print('wtf')
       coroutine.yield(nil)
     elseif request.filter and #request.filter > 0 then
       local results = require('libdash_nvim').query({
@@ -34,12 +33,13 @@ local function handle_selected(selected)
   end
 end
 
-function M.dash(bang)
+function M.dash(params)
   local current_file_type = vim.bo.filetype
   return require('snap').run({
-    producer = build_producer(current_file_type, bang or false),
+    producer = build_producer(current_file_type, params.bang or false),
     select = handle_selected,
     prompt = 'Dash>',
+    filter = params.initial_text or '',
   })
 end
 
