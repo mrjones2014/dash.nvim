d8c49e82df4136bce7d27ff5ad86d9ccd0ff29dd
Mat Jones <mat@mjones.network>
2022-03-01 15:22:43 -0500

    remove binaries from source control

--- scripts/install-for-architecture.bash
+++ scripts/install-for-architecture.bash
@@ -1,15 +1,26 @@
 #!/usr/bin/env bash
 
+set -e
+
 rm -rf ./lua/libdash_nvim.so ./lua/deps/
 
+DASH_ZIP_TO_DOWNLOAD=""
+DASH_LAST_TAG=$(git describe --tags --abbrev=0)
+DASH_UNZIP_DIR=""
+
 if [[ "$(uname -m)" == "arm64" ]]; then
-  cp ./bin/arm/libdash_nvim.so ./lua/
-  mkdir -p ./lua/deps/
-  cp -r ./bin/arm/deps/ ./lua/deps/
-  echo "Installed binary for ARM/M1 architecture"
+  DASH_ZIP_TO_DOWNLOAD="https://github.com/mrjones2014/dash.nvim/releases/download/$DASH_LAST_TAG/arm.zip"
+  DASH_UNZIP_DIR="arm"
 else
-  cp ./bin/x86/libdash_nvim.so ./lua/
-  mkdir -p ./lua/deps/
-  cp -r ./bin/x86/deps/ ./lua/deps/
-  echo "Installed binary for x86 architecture"
+  DASH_ZIP_TO_DOWNLOAD="https://github.com/mrjones2014/dash.nvim/releases/download/$DASH_LAST_TAG/x86.zip"
+  DASH_UNZIP_DIR="x86"
 fi
+
+rm -rf ./lua/arm/
+rm -rf ./lua/x86/
+curl -L "$DASH_ZIP_TO_DOWNLOAD" --output libdash.zip
+unzip libdash.zip -d ./lua/
+mv "./lua/$DASH_UNZIP_DIR/deps/" ./lua/
+mv "./lua/$DASH_UNZIP_DIR/libdash_nvim.so" ./lua/
+rm -rf "./lua/$DASH_UNZIP_DIR"
+rm -rf libdash.zip
